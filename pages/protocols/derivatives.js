f0aac003e131d4205ffb87dae5ebcb524997dfa6
Nuthan Prasad <nuthan1159@gmail.com>
2023-10-19 18:37:50 +0530

    funding rate

--- pages/protocols/derivatives.js
+++ pages/protocols/derivatives.js
@@ -31,14 +31,29 @@ export default function Derivatives() {
         //         return res.status(400).json({ success: false, message: "Couldn't fetch data" })
         //     });
 
+        // axios.get("https://api.gmx.io/tokens")
+        //     .then(function (response) {
+        //         // Handle successful response
+        //         console.log('BTC long max liquidity:', (response.data[1].data.maxGlobalLongSize - response.data[1].data.guaranteedUsd)/1e30);
+        //         console.log('BTC short max liquidity:', (response.data[1].data.maxGlobalShortSize - response.data[1].data.globalShortSize)/1e30);
+                
+        //         console.log('ETH  long max liquidity:', (response.data[2].data.maxGlobalLongSize - response.data[2].data.guaranteedUsd)/1e30);
+        //         console.log('ETH  short max liquidity:', (response.data[2].data.maxGlobalShortSize - response.data[2].data.globalShortSize)/1e30);
+        //     })
+        //     .catch(function (error) {
+        //         // Handle error
+        //         console.error('Error:', error);
+        //         return res.status(400).json({ success: false, message: "Couldn't fetch data" })
+        //     });
+
         axios.get("https://api.gmx.io/tokens")
             .then(function (response) {
                 // Handle successful response
-                console.log('BTC long max liquidity:', (response.data[1].data.maxGlobalLongSize - response.data[1].data.guaranteedUsd)/1e30);
-                console.log('BTC short max liquidity:', (response.data[1].data.maxGlobalShortSize - response.data[1].data.globalShortSize)/1e30);
+                console.log('BTC long funding rate:', (response.data[1].data.fundingRate)/1e4, "/hr");
+                console.log('BTC short funding rate:', (response.data[5].data.fundingRate)/1e4, "/hr");
                 
-                console.log('ETH  long max liquidity:', (response.data[2].data.maxGlobalLongSize - response.data[2].data.guaranteedUsd)/1e30);
-                console.log('ETH  short max liquidity:', (response.data[2].data.maxGlobalShortSize - response.data[2].data.globalShortSize)/1e30);
+                console.log('ETH  long funding rate:', (response.data[2].data.fundingRate)/1e4, "/hr");
+                console.log('ETH  short funding rate:', (response.data[5].data.fundingRate)/1e4, "/hr");
             })
             .catch(function (error) {
                 // Handle error
