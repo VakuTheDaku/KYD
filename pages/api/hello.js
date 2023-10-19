54b38d2a68099f7766fc327b288e510f9b863cf1
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2023-10-19 16:46:30 +0530

    fetch data

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const data = await axios.get("https://api.gmx.io/prices")
+    .then(function (response) {
+      // Handle successful response
+      console.log('Data:', response.data);
+      return res.status(200).json({ success: true, data: response.data })
+    })
+
+    .catch(function (error) {
+      // Handle error
+      console.error('Error:', error);
+      return res.status(400).json({ success: false, message: "Couldn't fetch data" })
+    });
+  return res.status(200).json({ name: 'John Doe' })
 }
--- pages/protocols/derivatives.js
+++ pages/protocols/derivatives.js
@@ -1,3 +1,7 @@
+import { Tab, Tabs } from "@nextui-org/react"
+import axios from "axios"
+import { useEffect } from "react"
+
 export default function Derivatives() {
     const protocols = [{
         name: "dydx",
@@ -13,10 +17,32 @@ export default function Derivatives() {
     {
         name: "ETH"
     }]
+    useEffect(() => {
+        axios.get("https://api.gmx.io/prices")
+            .then(function (response) {
+                // Handle successful response
+                console.log('Data:', response.data)
+            })
+            .catch(function (error) {
+                // Handle error
+                console.error('Error:', error);
+                return res.status(400).json({ success: false, message: "Couldn't fetch data" })
+            });
+    }, [])
     return (
         <div className="bg-black min-h-screen">
-            <div className="flex">
-                
+            <div className="flex w-full flex-col">
+                <Tabs aria-label="Options" color="primary" variant="bordered">
+                    <Tab
+                        key="photos"
+                        title={
+                            <div className="flex items-center space-x-2">
+
+                                <span>Photos</span>
+                            </div>
+                        }
+                    />
+                </Tabs>
             </div>
         </div>
     )
