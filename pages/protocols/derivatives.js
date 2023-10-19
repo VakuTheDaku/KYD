ac03c5baf7a458619e45c83933142f6375bb44dc
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2023-10-19 16:35:43 +0530

    new package axios

---
+++ pages/protocols/derivatives.js
@@ -0,0 +1,23 @@
+export default function Derivatives() {
+    const protocols = [{
+        name: "dydx",
+
+    },
+    {
+        name: "GMX"
+    }]
+
+    const coins = [{
+        name: "BTC"
+    },
+    {
+        name: "ETH"
+    }]
+    return (
+        <div className="bg-black min-h-screen">
+            <div className="flex">
+                
+            </div>
+        </div>
+    )
+}
