85a426347ddb0f99fcd8f90eff250ac79a17ade3
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2023-10-21 04:44:02 +0530

    build error fix

--- pages/connection.js
+++ pages/connection.js
@@ -2,14 +2,16 @@ const contractABI = require("./Contract.json");
 const YOUR_CONTRACT_ADDRESS = "0xA9e71dC950A7bb6eF64314b14F23b5a79ce7F468";
 import { ethers } from "ethers"
 const getContract = (window) => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    let contract = new ethers.Contract(
-        YOUR_CONTRACT_ADDRESS,
-        contractABI,
-        signer
-    );
-    return contract;
+    if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        let contract = new ethers.Contract(
+            YOUR_CONTRACT_ADDRESS,
+            contractABI,
+            signer
+        );
+        return contract;
+    }
 };
 
 export default getContract
